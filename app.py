0027a4178795fe2accca513d445eec01e577db10
Aviral Mansingka <aviralmansingka@gmail.com>
2017-02-22 17:19:49 -0500

    Added initial code for pymongo connection (tested), still needs proper structure

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
-from . import recommend
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
 
 @app.errorhandler(400)
 def not_found(error):
@@ -18,5 +16,48 @@ def not_found(error):
 
 @app.route('/api/recommend/<int:user_id>', methods = ['GET'])
 def get_recommendation(user_id):
-    recommend.get_recommendations()
+    get_recommendations(user_id)
     return 'Hello user # %d' % user_id
+
+def get_recommendations(user_id):
+    """
+    Responsible for returning the recommendations to the user
+    """
+
+    user_data = get_user_data(user_id)
+
+    all_data = get_user_data()
+
+def get_data(user_id):
+    pass
+
+def get_all_data():
+    pass
+
+class DataHandler:
+
+    def __init__(self):
+
+        self.client = MongoClient('mongodb://root:password@ds145639.mlab.com:45639/readly')
+        self.db = self.client.readly
+
+    def get_data(self, user_id=None):
+
+        users = self.db.users
+
+        if user_id is not None:
+
+            data = users.find_one({ '_id': user_id })
+
+        else:
+
+            cur = users.find()
+
+            data = []
+            for doc in cur:
+                data.append(doc)
+
+        return data
+
+
+
